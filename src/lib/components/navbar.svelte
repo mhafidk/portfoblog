330ed4f15056f191c85bc2eb260442b4afeb01e9
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2025-03-04 11:52:27 +0700

    add navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,12 @@
+<header>
+  <div class="">
+    <a href="/">Muhammad Hafid Khoirul</a>
+  </div>
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.css';
+	import Navbar from '$lib/components/navbar.svelte';
 	let { children } = $props();
 </script>
 
+<Navbar />
 {@render children()}
