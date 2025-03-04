2e90891d4aed5a55b57c5b72ccb823dd70dc1a0e
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2025-03-04 15:12:34 +0700

    add layout

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,6 @@
+<footer class="border-t border-gray-200 py-4 text-center text-sm">
+	<p>
+		© 2024 <span class="font-bold text-violet-600">Muhammad Hafid Khoirul</span>. All rights
+		reserved.
+	</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/components/navbar.svelte';
+	import Footer from '$lib/components/footer.svelte';
 	let { children } = $props();
 </script>
 
@@ -8,3 +9,4 @@
 <main class="mx-auto max-w-3xl px-4 py-8">
 	{@render children()}
 </main>
+<Footer />
