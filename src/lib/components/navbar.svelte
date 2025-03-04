57684b5fa487e3165356daa47dae8ce1fa6795e7
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2025-03-04 14:33:33 +0700

    adjust navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,12 +1,15 @@
 <header>
-  <div class="">
-    <a href="/">Muhammad Hafid Khoirul</a>
-  </div>
-  <nav>
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
-    </ul>
-  </nav>
+	<div class="border-b border-gray-200 py-6 text-center">
+		<a href="/" class="text-2xl font-bold hover:text-violet-700"
+			>Muhammad <span class="text-violet-600">Hafid</span> Khoirul</a
+		>
+	</div>
+	<nav>
+		<ul class="flex justify-center gap-8 border-b border-gray-200 py-4 text-sm">
+			<li><a href="/" class="hover:text-violet-700">Home</a></li>
+			<li><a href="/about" class="hover:text-violet-700">About</a></li>
+			<li><a href="/blog" class="hover:text-violet-700">Blog</a></li>
+			<li><a href="/#contact" class="hover:text-violet-700">Contact</a></li>
+		</ul>
+	</nav>
 </header>
