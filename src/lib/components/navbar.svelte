3705f6d98639f371b91eec0891e839320a9fcb73
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2025-04-03 19:14:46 +0700

    improve navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
 <header>
 	<div class="border-b border-gray-200 py-6 text-center">
 		<a href="/" class="text-xl font-light hover:text-lime-600 md:text-2xl"
@@ -6,10 +10,36 @@
 	</div>
 	<nav>
 		<ul class="flex justify-center gap-8 border-b border-gray-200 py-4 text-sm">
-			<li><a href="/" class="hover:text-lime-600">Home</a></li>
-			<li><a href="/about" class="hover:text-lime-600">About</a></li>
-			<li><a href="/blog" class="hover:text-lime-600">Blog</a></li>
-			<li><a href="/" class="hover:text-lime-600">Contact</a></li>
+			<li>
+				<a
+					href="/"
+					class="border-b-2 border-transparent pb-4 hover:text-lime-600"
+					class:text-lime-600={$page.url?.pathname === '/'}
+					class:border-lime-600={$page.url?.pathname === '/'}
+					class:border-transparent={$page.url?.pathname !== '/'}>Home</a
+				>
+			</li>
+			<li>
+				<a
+					href="/about"
+					class="border-b-2 border-transparent pb-4 hover:text-lime-600"
+					class:text-lime-600={$page.url?.pathname === '/about'}
+					class:border-lime-600={$page.url?.pathname === '/about'}
+					class:border-transparent={$page.url?.pathname !== '/about'}>About</a
+				>
+			</li>
+			<li>
+				<a
+					href="/blog"
+					class="border-b-2 border-transparent pb-4 hover:text-lime-600"
+					class:text-lime-600={$page.url?.pathname === '/blog'}
+					class:border-lime-600={$page.url?.pathname === '/blog'}
+					class:border-transparent={$page.url?.pathname !== '/blog'}>Blog</a
+				>
+			</li>
+			<li>
+				<a href="/" class="hover:text-lime-600">Contact</a>
+			</li>
 		</ul>
 	</nav>
 </header>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,4 @@
+<div class="mx-auto mt-8 max-w-xl">
+	<h2 class="text-2xl font-bold">Blog</h2>
+	<div class="mt-8"></div>
+</div>
