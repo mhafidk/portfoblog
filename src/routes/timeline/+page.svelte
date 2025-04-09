ce956aec98bc0b2008063a87e3108616005ec0b7
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2025-04-09 14:07:19 +0700

    add timeline page

---
+++ src/routes/timeline/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Timeline</title>
+</svelte:head>
+
+<div class="mx-auto mt-8 max-w-xl">
+	<h2 class="text-center text-2xl font-bold">Timeline</h2>
+	<div class="mt-8 text-center">
+		<p>Time flies, doesn't it?</p>
+	</div>
+</div>
