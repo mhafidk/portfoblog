4dd707f74b9afab0b237a2df796f279bf887cf3b
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2025-04-10 11:34:37 +0700

    add custom error page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/state';
 	import Navbar from '$lib/components/navbar.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	let { children } = $props();
 </script>
 
-<Navbar />
-<main class="px-4 py-8">
+{#if page.status == 200}
+	<Navbar />
+	<main class="px-4 py-8">
+		{@render children()}
+	</main>
+	<Footer />
+{:else}
 	{@render children()}
-</main>
-<Footer />
+{/if}
